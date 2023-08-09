4c2f46425ca7266e4be29c25a8ca357bf4ea3b16
Varga József Márk <vmark2145@gmail.com>
2023-08-09 18:26:17 +0300

    Finished documentating classes in the exception package.

--- src/main/java/com/markvarga21/usermanager/exception/ApiError.java
+++ src/main/java/com/markvarga21/usermanager/exception/ApiError.java
@@ -6,12 +6,35 @@ import org.springframework.http.HttpStatus;
 
 import java.util.Date;
 
+/**
+ * A custom class for sending useful information in a {@code ResponseEntity} when
+ * an exception occurs in the application.
+ */
 @Data
 @AllArgsConstructor
 public class ApiError {
+    /**
+     * The date when the error occurred.
+     */
     private Date timeStamp;
+
+    /**
+     * The status code of the error.
+     */
     private HttpStatus status;
+
+    /**
+     * The custom message of the error.
+     */
     private String message;
+
+    /**
+     * The operation type when the exception occurred.
+     */
     private OperationType operationType;
+
+    /**
+     * The full stacktrace of the exception.
+     */
     private String stackTrace;
 }
--- src/main/java/com/markvarga21/usermanager/exception/OperationType.java
+++ src/main/java/com/markvarga21/usermanager/exception/OperationType.java
@@ -1,5 +1,9 @@
 package com.markvarga21.usermanager.exception;
 
+/**
+ * A class for representing the operation which the user just did,
+ * thus causing an exception to raise.
+ */
 public enum OperationType {
     CREATE, READ, UPDATE, DELETE
 }
