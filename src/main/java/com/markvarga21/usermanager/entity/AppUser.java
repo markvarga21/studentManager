8366435e227509e2a78993e1c9fd1c644a314a1f
Varga József Márk <vmark2145@gmail.com>
2023-08-09 18:11:14 +0300

    Finished documentating the entity and dto classes.

--- src/main/java/com/markvarga21/usermanager/entity/AppUser.java
+++ src/main/java/com/markvarga21/usermanager/entity/AppUser.java
@@ -7,23 +7,65 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a user entity in the application.
+ */
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AppUser {
+    /**
+     * A unique identifier for the user.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
+
+    /**
+     * The first name of the user.
+     */
     private String firstName;
+
+    /**
+     * The last name of the user.
+     */
     private String lastName;
+
+    /**
+     * The birthdate name of the user.
+     */
     private LocalDate birthDate;
+
+    /**
+     * The birthplace of the user.
+     */
     @OneToOne(cascade = CascadeType.ALL)
     private Address placeOfBirth;
+
+    /**
+     * The nationality of the user.
+     */
     private String nationality;
+
+    /**
+     * The user's gender.
+     */
     private Gender gender;
+
+    /**
+     * The user's address.
+     */
     @ManyToOne(cascade = CascadeType.ALL)
     private Address address;
+
+    /**
+     * The user's email address.
+     */
     private String email;
+
+    /**
+     * The user's phone number.
+     */
     private String phoneNumber;
 }
