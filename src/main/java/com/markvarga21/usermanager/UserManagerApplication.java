bd8659e31d1321d1b9c0f0e9f238dbb0abf77727
Varga József Márk <vmark2145@gmail.com>
2023-08-09 18:41:33 +0300

    Finished documenting the rest of the classes.

--- src/main/java/com/markvarga21/usermanager/UserManagerApplication.java
+++ src/main/java/com/markvarga21/usermanager/UserManagerApplication.java
@@ -3,9 +3,14 @@ package com.markvarga21.usermanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A class holding the entrypoint of the application.
+ */
 @SpringBootApplication
 public class UserManagerApplication {
-
+	/**
+	 * The entrypoint of the application.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagerApplication.class, args);
 	}
--- src/main/java/com/markvarga21/usermanager/util/mapping/AppUserMapper.java
+++ src/main/java/com/markvarga21/usermanager/util/mapping/AppUserMapper.java
@@ -6,15 +6,31 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+/**
+ * A utility class which is used for mapping between the application's
+ * user entities and DTOs and backwards.
+ */
 @Component
 @RequiredArgsConstructor
 public class AppUserMapper {
     private final ModelMapper mapper;
 
+    /**
+     * Maps an {@code AppUserDto} to an {@code AppUser} entity.
+     *
+     * @param appUserDto the DTO object to be mapped to an {@code AppUser} entity class.
+     * @return the converted {@code AppUser} entity.
+     */
     public AppUser mapAppUserDtoToEntity(AppUserDto appUserDto) {
         return this.mapper.map(appUserDto, AppUser.class);
     }
 
+    /**
+     * Maps an {@code AppUser} entity to an {@code AppUserDto}.
+     *
+     * @param appUser the entity object to be mapped to an {@code AppUserDto} class.
+     * @return the converted {@code AppUserDto}.
+     */
     public AppUserDto mapAppUserEntityToDto(AppUser appUser) {
         return this.mapper.map(appUser, AppUserDto.class);
     }
