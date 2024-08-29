32c2f0e75148fd0af08e41731bb5105934692627
Varga József Márk <vmark2145@gmail.com>
2024-08-29 17:03:58 +0300

    Added accurate equals() method to the Report entity.

--- src/main/java/com/markvarga21/studentmanager/entity/Report.java
+++ src/main/java/com/markvarga21/studentmanager/entity/Report.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -55,4 +54,35 @@ public class Report {
      * The maximum length of the description.
      */
     static final int MAX_DESCRIPTION_LENGTH_IN_CHARACTERS = 1000;
+
+    /**
+     * This method is used to compare two reports
+     * if they are the same or not taking into account
+     * the issue date of the report.
+     *
+     * @param o The object to compare with.
+     * @return True if the reports are the same, false otherwise.
+     */
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Report report)) {
+            return false;
+        }
+        return this.issuerUsername.equals(report.issuerUsername)
+                && this.subject.equals(report.subject)
+                && this.description.equals(report.description);
+    }
+
+    /**
+     * This method is used to generate a hash code for the report.
+     *
+     * @return The hash code of the report.
+     */
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
